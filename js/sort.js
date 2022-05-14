0652a244bfab09b358e10dca5bccca8fb2b4db41
senmin <guoqianmin@leyaoyao.com>
2022-05-14 21:12:49 +0800

    feat: quickSort

--- js/sort.js
+++ js/sort.js
@@ -1,6 +1,5 @@
 // 排序算法
 
-
 // 1、冒泡
 function bubbleSort(arr) {
   let len = arr.length;
@@ -14,14 +13,13 @@ function bubbleSort(arr) {
       }
     }
   }
-  return arr
+  return arr;
 }
-
 
 // 2、选择排序   --  每次寻找将最小值交换位置
 function selectionSort(arr) {
   let len = arr.length;
-  let min = 0;//最小值的索引
+  let min = 0; //最小值的索引
 
   for (let i = 0; i < len - 1; i++) {
     min = i;
@@ -31,20 +29,19 @@ function selectionSort(arr) {
         min = j;
       }
     }
-    i === min ? null : [arr[i], arr[min]] = [arr[min], arr[i]];
+    i === min ? null : ([arr[i], arr[min]] = [arr[min], arr[i]]);
   }
 
   return arr;
 }
 
-
-// 3、插入排序  
+// 3、插入排序
 function insertionSort(arr) {
   let len = arr.length;
 
   for (let i = 1; i < len; i++) {
     let j = i;
-    let temp = arr[i]
+    let temp = arr[i];
     // 往前遍历，直至首位或 第一个小于temp 的值即止
     while (j > 0 && arr[j - 1] > temp) {
       arr[j] = arr[j - 1];
@@ -55,12 +52,29 @@ function insertionSort(arr) {
   return arr;
 }
 
+// 4 快速排序
+function quickSort(list) {
+  if (!list.length) {
+    return list;
+  }
 
+  let lesser = []; /* 比基准值小 */
+  let greater = []; /* 比基准大的 */
+  let pivot = list[0]; // 基准
 
+  for (let i = 1, n = list.length; i < n; i++) {
+    if (list[i] < pivot) {
+      lesser.push(list[i]);
+    } else {
+      greater.push(list[i]);
+    }
+  }
+  return quickSort(lesser).concat(pivot, quickSort(greater));
+}
 
 let testArr = [2, 1, 3, 1, 45, 5125, 2, 5, 24, 2, 45, 3, 54, 6, 67, 2, 34];
 //            [ 1, 1, 2, 2, 2, 2, 3, 3, 5, 6, 24, 34, 45, 45, 54, 67, 5125 ]
-console.log('bubbleSort---->', bubbleSort(testArr));
-console.log('selectionSort---->', selectionSort(testArr));
-console.log('insertionSort---->', insertionSort(testArr));
-
+// console.log("bubbleSort---->", bubbleSort(testArr));
+// console.log("selectionSort---->", selectionSort(testArr));
+// console.log("insertionSort---->", insertionSort(testArr));
+console.log("quickSort---->", quickSort(testArr));
